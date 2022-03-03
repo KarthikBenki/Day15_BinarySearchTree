30f0dc018eb994be75691b9a1d8639bc4ca6db67
karthikbenki <karthikmc007@gmail.com>
2022-03-03 14:12:05 +0530

    Added: Ability to search 63 in a given tree

--- src/Javahashtable/MYBinaryTreeTest.java
+++ src/Javahashtable/MYBinaryTreeTest.java
@@ -5,7 +5,7 @@ public class MYBinaryTreeTest {
         MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
         myBinaryTree.add(56);
         myBinaryTree.add(30);
-        myBinaryTree.add(70);   
+        myBinaryTree.add(70);
         myBinaryTree.add(22);
         myBinaryTree.add(40);
         myBinaryTree.add(11);
@@ -18,8 +18,13 @@ public class MYBinaryTreeTest {
         myBinaryTree.add(67);
 
         int size = myBinaryTree.getSize();
-        System.out.println("Size is : "+size);
+        System.out.println("Size is : " + size);
 
+        Integer res = myBinaryTree.searchKey(63);
 
+        if (res == 1)
+            System.out.println("key found");
+        else
+            System.out.println("key not found");
     }
 }
