1e71e37cc5dce3d3921e9270ffd71433e7f3df5f
karthikbenki <karthikmc007@gmail.com>
2022-03-03 10:55:09 +0530

    Added: Ability to create BST by adding 56 and then adding 30 & 70

---
+++ src/Javahashtable/MyBinaryNode.java
@@ -0,0 +1,26 @@
+package Javahashtable;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+    K key;
+    MyBinaryNode right;
+    MyBinaryNode left;
+
+    public MyBinaryNode(K key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+
+    }
+
+
+    public K getKey() {
+        return key;
+    }
+
+
+    public void setKey(K key) {
+        this.key=key;
+    }
+
+
+}
